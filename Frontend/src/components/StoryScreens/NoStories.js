7ddf3812f458d304f9df939f73c8591c3361bced
George-Beyrouti <georges.beyrouti@gmail.com>
2023-06-16 16:02:41 +0200

    no stories color

--- Frontend/src/components/StoryScreens/NoStories.js
+++ Frontend/src/components/StoryScreens/NoStories.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NoStories = () => (
-  <div className="no-story">
+  <div style={{color:"whitesmoke"}} className="no-story">
     <h2>No Story Found</h2>
     <p>Please try a different search term</p>
   </div>
